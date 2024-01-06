c34bd1a07ccfd2f795d795fd159340540fccdb9f
mohit-singh84 <f20211158@pilani.bits-pilani.ac.in>
2024-01-06 14:13:57 +0530

    first commit

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const loginRoute = require('./routes/login.route');
 const menuRoute = require('./routes/menu.route');
+const billsRoute = require('./routes/bills.route');
+const transactionRoute = require('./routes/transaction.route');
 const port = 5000;
 
 app.use(express.json());
 
 //Routes
 app.use("/api",loginRoute);
-app.use("/api",menuRoute)
+app.use("/api",menuRoute);
+app.use("/api",billsRoute);
+app.use("/api",transactionRoute);
 
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
